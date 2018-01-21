e6fac79b9844fc8997fe98f75d140854d189b1d6
Bassoum malick <mabassoum@gmail.com>
2018-01-21 15:27:10 +0000

    formulair programmatiq

--- src/app/components/movies/movie.service.ts
+++ src/app/components/movies/movie.service.ts
@@ -6,8 +6,8 @@ import {Movie} from "./movie";
 export class MovieService{
   movies:Movie[]=[
     new Movie('Game of thrones',['malick basoum','jules gra','seydou niane'], ['tidiane daff','jules ba'],1998),
-    new Movie('seigneur des anneaux',['babacar','ahmed','gueda ba'], ['jean samba','oumar ba'],2000),
-    new Movie('Abou',['abdoul','douda','seynabou seck'], ['alassane fall','jean pierre'],2005)
+    new Movie('Seigneur des anneaux',['babacar','ahmed','gueda ba'], ['jean samba','oumar ba'],2000),
+    new Movie('Men in black ',['abdoul','douda','seynabou seck'], ['alassane fall','jean pierre'],2005)
   ];
 
 
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from 'angular2/core';
 import {MovieService} from "./movie.service";
 import {Movie} from './movie';
+import {FormBuilder} from 'angular2/common';
+import {ControlGroup} from 'angular2/common';
+import {Validators} from 'angular2/common';
+
 
 @Component({
   selector:'movies',
@@ -14,18 +18,41 @@ export class Movies implements OnInit{
   movieService:MovieService;
   recentlyWatchMovies:Movie[];
   nbOfMovie: Number;
+  movie: Movie;
+  myForm:ControlGroup;
+  formBuilder: FormBuilder;
 
-  constructor(movieService: MovieService){
+  constructor(movieService: MovieService, formBuilder: FormBuilder){
     this.movieService=movieService;
+    this.formBuilder=formBuilder;
 
   }
 
-  addMovie(movie){
-    this.movieService.addMovie(movie);
+  addMovie(){
+    console.log(this.myForm);
+    let formValue=this.myForm.value;
+
+    this.movie= new Movie();
+    this.movie.title=formValue.title;
+    this.movie.actors=formValue.actors;
+    this.movie.directors=formValue.directors;
+    this.movie.year=formValue.year;
+
+    this.movieService.addMovie(this.movie);
     this.nbOfMovie=this.recentlyWatchMovies.length;
+
+    console.log(this.recentlyWatchMovies);
+    this.movie= new Movie();
   }
   ngOnInit(){
     this.recentlyWatchMovies=this.movieService.getAllMovies();
     this.nbOfMovie=this.recentlyWatchMovies.length;
+
+    this.myForm=this.formBuilder.group({
+      'title':['', Validators.required],
+      'actors': [['']],
+      'directors': [['']],
+      'year': ['2016', Validators.required]
+    })
   }
 }
